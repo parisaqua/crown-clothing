47d655cd8a92e5b82a220f1007011c6640ec70a6
Fred <frederic@pmo.digital>
2020-01-25 08:46:06 +0100

    Etape 74

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSignUp = () => (
+    <div className='sing-in-and-sing-up'>SIGN IN</div>
+);
+
+export default SignInAndSignUp;
+
