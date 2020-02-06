45a34fc51303af033a01ed5fab041523c23576c2
Frédéric BASTIAN <frederic@bastian.paris>
2020-02-06 07:25:58 +0100

    Etape 117

--- src/components/cart-dropdown/cart-dropdown-component.jsx
+++ src/components/cart-dropdown/cart-dropdown-component.jsx
@@ -3,6 +3,8 @@ import {connect} from 'react-redux';
 import './cart-dropdown.styles.scss';
 import CustomButton from '../custom-button/custom-button.component';
 
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+
 import CartItem from '../cart-item/cart-item.component';
 
 const CartDropdown = ({ cartItems }) => (
@@ -16,8 +18,8 @@ const CartDropdown = ({ cartItems }) => (
     </div>
 )
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-    cartItems
+const mapStateToProps = state => ({
+    cartItems: selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(CartDropdown);
