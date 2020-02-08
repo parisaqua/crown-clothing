4ab96c020d41ee785b1e8603208d3a55329f200c
Fred <frederic@pmo.digital>
2020-02-08 10:28:17 +0100

    Etape 123

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
 
 const CheckoutPage = ({cartItems, total}) => (
-   
+
     <div className='checkout-page'>
         <div className='checkout-header'>
             <div className='header-block'>
@@ -29,7 +31,7 @@ const CheckoutPage = ({cartItems, total}) => (
         </div>
         {
             cartItems.map(cartItem =>
-                cartItem.className
+                <CheckoutItem key={cartItem.id} cartItem={cartItem} />
                 )
         }
         <div className='total'>
